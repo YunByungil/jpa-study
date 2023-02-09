9683e00a4bc7edf9a93cbd0d930d37d306ef14ec
YunByungil <dbsquddlfz@naver.com>
2023-02-09 18:41:36 +0900

    회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,11 +8,58 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    /**
+     * 회원 조회 API
+     * ArrayList를 바로 반환하면 안 됨.
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+
+        List<MemberDTO> collect = findMembers.stream()
+                .map(m -> new MemberDTO(m.getName()))
+                .collect(Collectors.toList());
+
+//        List<MemberDTO> collect = new ArrayList<>();
+//        for (Member m : findMembers) {
+//            MemberDTO memberDTO = new MemberDTO(m.getName());
+//            collect.add(memberDTO);
+//        }
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data; // 타입 명시하자.
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDTO {
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Validated Member member) {
@@ -39,11 +86,6 @@ public class MemberApiController {
         memberService.update(id, request.getName());
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
-    }
-
-    @Data
-    static class UpdateMemberRequest {
-        private String name;
     }
     @Data
     @AllArgsConstructor
